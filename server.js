f45643786f562798261e37bba5fa93408a307157
Degubi <gergonoorbi@gmail.com>
2022-04-23 09:32:22 +0200

    Add heroku deploy github action

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ import { resolve } from 'path';
 
 const server = express();
 const workDir = resolve() + '/docs';
-const port = process.env.PORT ?? 8080;
 
 server.use(express.static(workDir));
 server.get('/', (_, res) => res.sendFile(`${workDir}/index.html`));
-server.listen(port);
-
-console.log(`http://localhost:${port}`);
+server.listen(process.env.PORT ?? 8080);
