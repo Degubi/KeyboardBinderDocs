bf4db708fc3fcd1a11c0a1b2a65ce5629c8de0d9
Degubi <gergonoorbi@gmail.com>
2022-09-25 22:00:17 +0200

    Update ppro docs

--- apiMappings.py
+++ apiMappings.py
@@ -39,6 +39,7 @@ def get_example_arg_mappings(function_name: str, module_name: str, overload_name
         case ('PremierePro', 'set_scale_to_frame_size_on_selection', _): return { 'enabled': True }
         case ('PremierePro', 'add_audio_effect_to_selection', _): return { 'effect_name': 'Bass' }
         case ('PremierePro', 'add_video_effect_to_selection', _): return { 'effect_name': 'Gamma Correction' }
+        case ('PremierePro', 'insert_item_at_player_position', _): return { 'item_path': 'Bin1/Bin2/ClipOrSequenceName' }
         case ('Twitch', 'create_clip', _): return { 'channel_name': 'shroud' }
         case ('Windows', 'copy_text_to_clipboard', _): return { 'text': 'epic text' }
         case ('Windows', 'exec_command', _): return { 'command': 'explorer https://google.com' }
